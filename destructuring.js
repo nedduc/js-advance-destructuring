426f192f75d24b72fec96a24eb485ea4f29d7679
nedduc <marinacudden@gmail.com>
2021-10-22 13:31:19 +0000

    Adance JacaScript destructuring

--- destructuring.js
+++ destructuring.js
@@ -4,9 +4,42 @@
  */
 
 // Destructuring arrays
+let ages = [30, 26, 27];
+// let john = ages[0];
+// let mary = ages[1];
+// let joe = ages[2];
+let [john, mary, joe] = ages;
+console.log(john, mary,joe)
 
 // Destructuring objects
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia:"accountant",
+};
+let {mike, jill, alicia} = jobs;
+console.log(mike, jill, alicia);
 
 // Destructuring subsets
+let languages = ["english", "french", "spanish", "german", "japanese"];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary);
+let [, , maryNative, maraySecondary] = languages;
+console.log(maryNative, maraySecondary);
+
+let languages2 = {
+    firstLanguage: "english",
+    secondLanguage: "french",
+    thirdLanguage: "german",
+    fourthLanguage: "japanese",
+};
+let{firstLanguage, thirdLanguage} = languages2;
+console.log(firstLanguage, thirdLanguage);
 
 // Using rest parameter syntax
+let fruits = ["apple", "orange", "banana", "peach", "cherry"];
+let [favourite, secondFavorite, ...others] = fruits;
+console.log(favourite);
+console.log(secondFavorite);
+console.log(others);
+
